3c40231af5f3f08d29dd9cc50b9aa18332602cfd
Sanam <sanamxdeveloper@gmail.com>
2025-11-25 11:49:17 +0545

    hero-section

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import Search from './components/search'
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  return (
+    <main>
+      <div className="pattern"/>
+
+      <div className="wrapper">
+        <header>  
+          <img src="./hero-img.png" alt="hero banner"/>
+          <h1>
+            Find <span className='text-gradient'> Movies </span> You'll Enjoy Without the Hassle
+          </h1>
+        </header>
+        
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className='text-white'>{searchTerm}</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/search.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+
+const search = ({searchTerm, setSearchTerm}) => {
+  return (
+    <div className="search">
+        <div>
+          <img src="search.svg" alt="search" />
+          <input 
+              type="text"
+              placeholder='Search through thousands of movies' 
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)} 
+          />
+        </div>
+      </div>
+
+
+
+  )
+}
+
+export default search
